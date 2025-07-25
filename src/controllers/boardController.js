bce9990165081a39372202642a9e8e5f55c9b984
ducchien01 <chien23964@nuce.edu.vn>
2025-07-25 16:26:00 +0700

    Error Handling

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -2,12 +2,12 @@ import { StatusCodes } from 'http-status-codes'
 
 const createNew = async (req, res, next) => {
   try {
-    console.log('req.body: ', req.body)
-    console.log('req.query: ', req.query)
-    console.log('req.params: ', req.params)
-    console.log('req.files: ', req.files)
-    console.log('req.cookies: ', req.cookies)
-    console.log('req.jwtDecoded: ', req.jwtDecoded)
+    // console.log('req.body: ', req.body)
+    // console.log('req.query: ', req.query)
+    // console.log('req.params: ', req.params)
+    // console.log('req.files: ', req.files)
+    // console.log('req.cookies: ', req.cookies)
+    // console.log('req.jwtDecoded: ', req.jwtDecoded)
 
     // Điều hướng dữ liệu sang tầng Service
 
